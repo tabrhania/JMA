7f6a53855e63da9dfbbb2bbaa06ab71096b7ea3a
NauvalPermana <47969857+NauvalPermana@users.noreply.github.com>
2019-10-20 21:06:38 +0700

    Update trader_profile.php

--- application/views/user/trader_profile.php
+++ application/views/user/trader_profile.php
@@ -2,6 +2,8 @@
 <?php $this->load->view('template/navbar') ?>
 <!-- isi halaman memulai -->
 
+<div class="container"><h1 class="center">Profile Copy Trader</h1></div>
+
 <div class="container">
         <iframe frameborder="0" width="220" height="140" src="https://www.mql5.com/en/signals/widget/signal/4722?t=red"></iframe>
         <iframe frameborder="0" width="220" height="140" src="https://www.mql5.com/en/signals/widget/signal/4726"></iframe>
