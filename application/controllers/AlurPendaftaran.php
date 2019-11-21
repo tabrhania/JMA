02fbfbde1fdd9dc66c6e894e2f03d573b262dc65
khansaidr24 <45646505+khansaidr24@users.noreply.github.com>
2019-11-21 17:01:36 +0700

    flowchart

--- application/controllers/AlurPendaftaran.php
+++ application/controllers/AlurPendaftaran.php
@@ -3,7 +3,11 @@
 class AlurPendaftaran extends CI_Controller {
 
     public function index() {
-        $this->load->view("user/alur_pendaftaran");
+        $this->load->view('template/page_head');
+        $this->load->view('template/navbar');
+        $this->load->view('user/alur_pendaftaran');
+        $this->load->view('template/footer');
+        $this->load->view('template/js');
     }
 
 }
--- application/views/user/alur_pendaftaran.php
+++ application/views/user/alur_pendaftaran.php
@@ -1,7 +1,4 @@
-<?php $this->load->view('template/page_head') ?>
-<?php $this->load->view('template/navbar') ?>
-<!-- isi halaman memulai -->
-
-<!-- isi halaman end -->
-<?php $this->load->view('template/footer') ?>
-<?php $this->load->view('template/js') ?>
+							<div class="hero-slides-thumb]" data-animation="fadeInUp" data-delay="1000ms" >
+								<img style="width: 50%;display: block;margin: auto;"src="<?=base_url() ?>/assets/img/alur-img/flowchart.jpg" alt="">
+							</div>
+							<p>
